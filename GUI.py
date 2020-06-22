284e949f5cacd9b9383ceacc6a7afde3f781979b
jaimejusto <jaimejusto@gmail.com>
2020-06-21 20:11:46 -0700

    updated description

--- GUI.py
+++ GUI.py
@@ -1,22 +1,39 @@
-from tkinter import *
+import pygame as pyg
+from sys import exit
 
-# window
-window = Tk()
 
-# window title
-window.title("Gess Board Game")
+pyg.init()
 
-# window dimensions
-window.configure(width=800, height=450)
+# screen
+screen_width = 650
+screen_height = 550
 
-# window background color
-window.configure(bg="tan4")
+# colors
+white = (255, 255, 255)
+black = (0, 0, 0)
 
-# center the window
-windowWidth = window.winfo_reqwidth()
-windowHeight = window.winfo_reqheight()
-positionRight = int(window.winfo_screenwidth() / 2 - windowWidth / 2)
-positionLeft = int(window.winfo_screenheight() / 2 - windowHeight / 2)
-window.geometry("+{}+{}".format(positionRight, positionLeft))
+# wood background
+woodImage = pyg.image.load("wood_board.jpg")
 
-window.mainloop()
+
+surface = pyg.display.set_mode((screen_width, screen_height))
+pyg.display.set_caption("Gess Game")
+surface.fill(white)
+
+def wood(x, y):
+    surface.blit(woodImage, (x, y))
+
+pyg.Surface.scroll()
+# close window by clicking X
+while True:
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            pyg.quit()
+            exit()
+        if event.type == pyg.KEYDOWN:
+            pyg.quit()
+            exit()
+    pyg.display.update()
+
+
+
--- GessGame.py
+++ GessGame.py
@@ -2,29 +2,6 @@
 # Date: 5/23/2020
 """ Description:
     A board game for two players that is a combination of Chess and Go.
-    Rules:
-    Starting with Black, players take turns to move their piece on the board.
-    A move must change the stone configuration, therefore, a player is not allowed to skip their turn.
-    A piece consists of a 3x3 grid of squares, called the footprint, which may only be moved by the player whose stones
-    are within the 3x3 grid.
-    A piece cannot contain stones of the other player.
-    A piece must contain at least one stone around the central square.
-    Each piece can move based on the arrangement of stones in its footprint:
-        If center of the footprint is unoccupied, it may only move up to three spaces; if the center is occupied, it may
-        move any number of spaces as long as there is no stone blocking its path.
-        The surrounding squares determine the direction the piece can move. If a square is occupied by a stone, the
-        piece may move in the direction indicated by the square's location relative to the central square; if a square
-        is unoccupied, the piece cannot move in that direction.
-    As a piece moves, all of the stones in its footprint move in unison.
-    When the footprint overlaps with any other stones on the board, those stones are removed from the board and the move
-    ends.
-    If the footprint moves partially out of the board, the move ends. The stones of the piece which are on the square
-    that has moved out of the board are removed.
-    A move may also end before any stone is removed.
-    The objective of the game is to be the only player with a ring piece on the board. If a player has no ring pieces on
-    the board after their turn, that player loses the game. If neither player has a ring piece, the player who has just
-    moved loses the game.
-        A ring is any piece consisting of eight stones around an empty central square.
 """
 
 
