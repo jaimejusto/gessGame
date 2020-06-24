62c6a68202c721b7d353dd2e84ec62381ba7c003
jaimejusto <jaimejusto@gmail.com>
2020-06-23 23:35:40 -0700

    outputting board with lines

--- GUI.py
+++ GUI.py
@@ -1,24 +1,72 @@
-import pygame as pyg
-from sys import exit
+import pygame
+import sys
+import constants
 
 
+def initialize_game():
+    pygame.init()
+    surface = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    pygame.display.set_caption(constants.TITLE)
+    surface.fill(constants.WHITE)
+    return surface
+
+
+def read_board(gridfile):
+    with open(gridfile, "r") as f:
+        board_map = f.readlines()
+    board_map = [line.strip() for line in board_map]
+    return board_map
+
+
+def get_space_color(space_contents):
+    space_color = constants.DARKLAVA
+    if space_contents == "X":
+        space_color = constants.PASTELBROWN
+    if space_contents == "-":
+        space_color = constants.PALETAUPE
+    return space_color
+
+
+def draw_board(surface, grid_spaces):
+    for j, space in enumerate(grid_spaces):
+        for i, space_contents in enumerate(space):
+            #print("{},{}: {}".format(i, j, space_contents))
+            myrect = pygame.Rect(i*constants.BLOCK_WIDTH, j*constants.BLOCK_HEIGHT, constants.BLOCK_WIDTH, constants.BLOCK_HEIGHT)
+            pygame.draw.rect(surface, get_space_color(space_contents), myrect)
+
+
+def draw_lines(surface):
+    for i in range(constants.GRID_ROWS):
+        new_height = round(i * constants.BLOCK_HEIGHT)
+        new_width = round(i * constants.BLOCK_WIDTH)
+        pygame.draw.line(surface, constants.DARKLAVA, (0, new_height), (constants.SCREEN_WIDTH, new_height), 2)
+        pygame.draw.line(surface, constants.DARKLAVA, (new_width, 0), (new_width, constants.SCREEN_HEIGHT), 2)
+
+def game_loop(surface, board_grid):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                pygame.quit()
+                sys.exit()
+        draw_board(surface, board_grid)
+        draw_lines(surface)
+        pygame.display.update()
+
+
+def main():
+    board_map = read_board(constants.GRIDFILE)
+    surface = initialize_game()
+    game_loop(surface, board_map)
+"""
 pyg.init()
 
-# screen
-screen_width = 650
-screen_height = 550
 
-# colors
-white = (255, 255, 255)
-black = (0, 0, 0)
 
 # wood background
 woodImage = pyg.image.load("wood_board.jpg")
-
-
-surface = pyg.display.set_mode((screen_width, screen_height))
-pyg.display.set_caption("Gess Game")
-surface.fill(white)
 
 
 def wood(x, y):
@@ -28,15 +76,8 @@ def wood(x, y):
 x = (screen_width // 2000)
 y = (screen_height // 2000)
 
-# close window by clicking X
-while True:
-    for event in pyg.event.get():
-        if event.type == pyg.QUIT:
-            pyg.quit()
-            exit()
-        if event.type == pyg.KEYDOWN:
-            pyg.quit()
-            exit()
-    pyg.display.update()
-    wood(x, y)
 
+"""
+
+if __name__ == "__main__":
+    main()
